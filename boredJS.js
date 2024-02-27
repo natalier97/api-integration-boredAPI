674bfd7456cb6b51a7ec9c166d93d180436e4868
Natalie <rivero_natalie@outlook.com>
2024-02-27 15:28:33 -0500

    completed JS and HTML

---
+++ boredJS.js
@@ -0,0 +1,25 @@
+const randomActivity = async (event) => {
+  try {
+    event.preventDefault();
+
+    // Define API endpoint URL (response)
+    let response = await fetch("http://www.boredapi.com/api/activy/");
+    // Format response to json
+    let responseData = await response.json();
+    // Assign activity (response.activity) to variable
+    let activity = responseData.activity;
+    // Assign webpage location to variable
+    let pOutput = document.getElementById("output");
+    // Change innerText of variable to activity variable
+    if (activity) {
+      pOutput.innerText = activity;
+    } else {
+      pOutput.innerText = responseData.error;
+    }
+  } catch (err) {
+    // Due to the nature of the API this will never execute
+    //what do we want to console.log for errors?
+    // console.err(err.message);
+    console.log(err.message);
+  }
+};
